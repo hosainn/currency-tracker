a936389c6363fbb0edb6fdabdade3efa7637080d
Saddam Hosain <hosains738@gmail.com>
2024-06-16 02:51:57 +0600

    feat: Implement currency informer lambda

--- lambdas/currency_informer/currency_informer.py
+++ lambdas/currency_informer/currency_informer.py
@@ -2,13 +2,97 @@
 
 import json
 import logging
+import os
+from datetime import datetime, timedelta, timezone
+
+import boto3
+import pytz
+from boto3.dynamodb.conditions import Key
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+DYNAMODB = boto3.resource('dynamodb')
+TABLE_NAME = os.environ.get("TABLE_NAME")
+
+EXCHANGE_RATES_TABLE = DYNAMODB.Table(TABLE_NAME)
+
+
+def get_exchange_rates_status(current_rate, previous_rate):
+    """asdf"""
+
+    if current_rate is None:
+        status = "not_available"
+    elif previous_rate is None:
+        status = "not_available"
+    elif current_rate > previous_rate:
+        status = "high"
+    elif current_rate < previous_rate:
+        status = "low"
+    else:
+        status = "equal"
+
+    return status
+
+def get_exchange_rates(date):
+    """dadsf"""
+    response = EXCHANGE_RATES_TABLE.query(
+        KeyConditionExpression=Key('date').eq(date)
+    )
+    if len(response["Items"]) and response["Items"][0]:
+        rates = response['Items'][0].get("rates", {})
+        return rates
+    return {}
+
+def process_currency_information():
+    """asdfasdf"""
+    cet_timezone = pytz.timezone('CET')
+    current_cet_datetime = datetime.now(timezone.utc).replace(
+        microsecond=0, tzinfo=pytz.utc).astimezone(cet_timezone
+                                                   )
+    timestamp_cet = datetime.now(timezone.utc).replace(
+        microsecond=0, tzinfo=pytz.utc).astimezone(cet_timezone
+    ).isoformat()
+    current_date_only = current_cet_datetime.strftime('%Y-%m-%d')
+    previous_date_only = (current_cet_datetime - timedelta(days=1)).strftime('%Y-%m-%d')
+
+    response = {
+        "timestamp_cet": timestamp_cet,
+        "current_date": current_date_only,
+        "previous_date": previous_date_only,
+        "exchange_rates": {}
+    }
+
+    current_rates = get_exchange_rates(current_date_only)
+    previous_rates = get_exchange_rates(previous_date_only)
+
+
+    for currency in current_rates.keys():
+        current_rate = current_rates.get(currency)
+        previous_rate = previous_rates.get(currency)
+
+        status = get_exchange_rates_status(current_rate, previous_rate)
+
+        response["exchange_rates"][currency] = {
+            "current_rate": float(current_rate) if current_rate is not None else None,
+            "previous_rate": float(previous_rate) if previous_rate is not None else None,
+            "status": status
+        }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
+
 
 def handler(event, _):
-    """Lambda handler"""
-    logger.info(json.dumps(event))
+    """Currency rate informer lambda handler"""
+    if event["httpMethod"] == "GET":
+        #TODO: Handle exception
+        return process_currency_information()
 
-    return json.dumps(event)
+    return {
+        'statusCode': 405,
+        'body': json.dumps({"message": "Method Not Allowed"}),
+        'headers': {'Allow': 'GET'}
+    }
--- terraform/currency_informer_lambda.tf
+++ terraform/currency_informer_lambda.tf
@@ -13,6 +13,11 @@ resource "aws_lambda_function" "currency_informer_lambda" {
   source_code_hash = data.archive_file.currency_informer_lambda_logslambda_zip_file.output_base64sha256
   runtime          = local.lambda_python_version
   layers           = [aws_lambda_layer_version.common_lambda_layer.arn]
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.currency_history_table.name
+    }
+  }
 }
 
 resource "aws_iam_role" "currency_informer_lambda_iam_role" {
@@ -29,4 +34,26 @@ resource "aws_cloudwatch_log_group" "currency_informer_lambda_log_group" {
 resource "aws_iam_role_policy_attachment" "currency_informer_lambda_policy" {
   role       = aws_iam_role.currency_informer_lambda_iam_role.name
   policy_arn = aws_iam_policy.lambda_log_policy.arn
+}
+
+
+resource "aws_iam_role_policy" "currency_informer_lambda_iam_policy" {
+  name   = "currency-informer-lambda-iam-policy"
+  role   = aws_iam_role.currency_informer_lambda_iam_role.name
+  policy = data.aws_iam_policy_document.currency_informer_lambda_iam_policy_document.json
+}
+
+data "aws_iam_policy_document" "currency_informer_lambda_iam_policy_document" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:Query",
+    ]
+    resources = [
+      aws_dynamodb_table.currency_history_table.arn
+    ]
+  }
+
 }
